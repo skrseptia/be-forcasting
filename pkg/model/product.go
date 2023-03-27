c46840bb1541668acc60f9cd984730f51c8c9bd6
ikhsan <ikhsan.guntara@magnatech.co.id>
2023-03-27 12:16:17 +0700

    update

--- pkg/model/product.go
+++ pkg/model/product.go
@@ -2,10 +2,10 @@ package model
 
 type Product struct {
 	Model
-	MerchantID  uint    `json:"merchant_id" binding:"required"`
+	MerchantID  string  `json:"merchant_id" binding:"required"`
 	Name        string  `json:"name" binding:"required"`
 	Description string  `json:"description" binding:"required"`
 	ImageURL    string  `json:"image_url"`
 	Price       float64 `json:"price" binding:"required"`
-	Category    string  `json:"category" binding:"required"`
+	CategoryID  string  `json:"category_id" binding:"required"`
 }
