5ce86d1dae6759536547f89e149f7b8e986d6cc5
Dhimas Prajaya <dhimasprajaya@gmail.com>
2023-05-14 19:44:25 +0700

    Create Summary Dashboard

--- pkg/model/dashboard.go
+++ pkg/model/dashboard.go
@@ -1,9 +1,67 @@
 package model
 
+import "time"
+
 type Dashboard struct {
+	Summary         Summary           `json:"summary"`
+	CustomerTrx     []CustomerTrx     `json:"customer_trx"`
+	StockAlert      []StockAlert      `json:"stock_alert"`
+	Top10Trx        []Top10Trx        `json:"top_10_trx"`
+	Top5Product     []Top5Product     `json:"top_5_product"`
+	DailyTrxChart   []DailyTrxChart   `json:"daily_trx_chart"`
+	MonthlyTrxChart []MonthlyTrxChart `json:"monthly_trx_chart"`
+}
+
+type Summary struct {
 	TotalCategory    int64 `json:"total_category"`
 	TotalProduct     int64 `json:"total_product"`
 	TotalTransaction int64 `json:"total_transaction"`
 	TotalUOM         int64 `json:"total_uom"`
 	TotalCustomer    int64 `json:"total_customer"`
+}
+
+type CustomerTrx struct {
+	Customer   string  `json:"customer"`
+	TotalTrx   int     `json:"total_trx"`
+	AmountTrx  float64 `json:"amount_trx"`
+	AverageTrx float64 `json:"average_trx"`
+}
+
+type StockAlert struct {
+	ID   int    `json:"id"`
+	Code string `json:"code"`
+	Name string `json:"name"`
+	Qty  int    `json:"qty"`
+	UOM  string `json:"uom"`
+}
+
+type Top10Trx struct {
+	TrxID     string    `json:"trx_id"`
+	CreatedAt time.Time `json:"-"`
+	TrxDate   string    `json:"trx_date"`
+	CreatedBy string    `json:"created_by"`
+	Customer  string    `json:"customer"`
+	Total     float64   `json:"total"`
+}
+
+type Top5Product struct {
+	ID            int     `json:"id"`
+	Name          string  `json:"name"`
+	Price         float64 `json:"price"`
+	TotalQty      int     `json:"total_qty"`
+	AverageAmount float64 `json:"average_amount"`
+	TotalAmount   float64 `json:"total_amount"`
+}
+
+type MonthlyTrxChart struct {
+	Month  string  `json:"month"`
+	Year   int     `json:"year"`
+	Amount float64 `json:"amount"`
+}
+
+type DailyTrxChart struct {
+	Day    int     `json:"day"`
+	Month  string  `json:"month"`
+	Year   int     `json:"year"`
+	Amount float64 `json:"amount"`
 }
--- pkg/storage/mysql/reports.go
+++ pkg/storage/mysql/reports.go
@@ -1,25 +1,27 @@
 package mysql
 
 import (
+	"food_delivery_api/cfg"
 	"food_delivery_api/pkg/model"
+	"time"
 )
 
 func (s *Storage) ReadReportDashboard() (model.Dashboard, error) {
 	var obj model.Dashboard
 	var err error
-	var ttlCtg, ttlPrd, ttlTrx, ttlUom, ttlCst int64
 
+	// forming summary
+	var ttlCtg, ttlPrd, ttlTrx, ttlUom, ttlCst int64
 	err = s.db.Model(&model.Category{}).Count(&ttlCtg).Error
 	err = s.db.Model(&model.Product{}).Count(&ttlPrd).Error
 	err = s.db.Model(&model.Transaction{}).Count(&ttlTrx).Error
 	err = s.db.Model(&model.UOM{}).Count(&ttlUom).Error
 	err = s.db.Model(&model.Transaction{}).Distinct("customer").Count(&ttlCst).Error
-
 	if err != nil {
 		return obj, err
 	}
 
-	obj = model.Dashboard{
+	sum := model.Summary{
 		TotalCategory:    ttlCtg,
 		TotalProduct:     ttlPrd,
 		TotalTransaction: ttlTrx,
@@ -27,5 +29,66 @@ func (s *Storage) ReadReportDashboard() (model.Dashboard, error) {
 		TotalCustomer:    ttlCst,
 	}
 
+	// forming customer trx
+	var ct []model.CustomerTrx
+	s.db.Raw(`select customer, count(*) as total_trx, sum(total) as amount_trx, avg(total) as average_trx 
+		from transactions group by customer`).
+		Scan(&ct)
+
+	// forming stock alert
+	var sa []model.StockAlert
+	s.db.Raw(`select p.id, p.code, p.name, p.qty, u.name as UOM from products p join uoms u on u.id = p.uom_id
+		where qty < 100 order by qty;`).
+		Scan(&sa)
+
+	// forming top 10 trx
+	var t10 []model.Top10Trx
+	s.db.Raw(`select created_at, trx_id, created_by, customer, total from transactions order by total desc limit 10`).
+		Scan(&t10)
+
+	for i, v := range t10 {
+		t10[i].TrxDate = v.CreatedAt.Format(cfg.AppTLayout)
+	}
+
+	// forming top 5 product
+	var t5p []model.Top5Product
+	s.db.Raw(`select product_id as id, name, price, sum(qty) as total_qty, sum(sub_total) / sum(qty) as average_amount,
+    	sum(sub_total) as total_amount from transaction_lines group by product_id, name, price limit 5`).
+		Scan(&t5p)
+
+	// forming daily trx chart
+	var dc []model.DailyTrxChart
+	s.db.Raw(`select day(created_at) as day, month(created_at) as month, year(created_at) as year, sum(total) as amount
+		from transactions group by day(created_at), month(created_at), year(created_at)`).
+		Scan(&dc)
+
+	for i, v := range dc {
+		dc[i].Month = convertMonth(v.Month)
+	}
+
+	// forming monthly trx chart
+	var mc []model.MonthlyTrxChart
+	s.db.Raw(`select month(created_at) as month, year(created_at) as year, sum(total) as amount from transactions group by year(created_at), month(created_at)`).
+		Scan(&mc)
+
+	for i, v := range mc {
+		mc[i].Month = convertMonth(v.Month)
+	}
+
+	obj = model.Dashboard{
+		Summary:         sum,
+		CustomerTrx:     ct,
+		StockAlert:      sa,
+		Top10Trx:        t10,
+		Top5Product:     t5p,
+		DailyTrxChart:   dc,
+		MonthlyTrxChart: mc,
+	}
+
 	return obj, nil
+}
+
+func convertMonth(strMonth string) string {
+	date, _ := time.Parse("1", strMonth)
+	return date.Month().String()
 }
