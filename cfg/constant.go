09d3bfae998a91e565179c7ffb96cd225bf384a8
Dhimas Prajaya <dhimasprajaya@gmail.com>
2023-07-13 23:46:28 +0700

    Update Config DB

--- cfg/constant.go
+++ cfg/constant.go
@@ -1,7 +1,7 @@
 package cfg
 
 const (
-	AppName     = "food-delivery-api"
+	AppName     = "andhiga_pupuk_api"
 	AppDLayout  = "2006-01-02"
 	AppTLayout  = "2006-01-02 15:04:05"
 	AppTimeZone = "Asia/Jakarta"
